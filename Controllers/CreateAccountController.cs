1a870d213be03e0ccb8a2ef9acdc7322794e0468
albadev <joseadauto923@gmail.com>
2022-10-02 18:23:04 -0300

    Create account

---
+++ Controllers/CreateAccountController.cs
@@ -0,0 +1,35 @@
+﻿using BlogDoDautin.Models;
+using BlogDoDautin.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogDoDautin.Controllers
+{
+    public class CreateAccountController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        public CreateAccountController(IUserRepository user)
+        {
+            _userRepository = user;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateNewUser(UserModel usermodel)
+        {
+            try
+            {
+                _userRepository.createUser(usermodel);
+                TempData["success"] = "Criado com sucesso!";
+                return RedirectToAction("Index");
+            }
+            catch(Exception error)
+            {
+                throw new Exception("Erro" + error.ToString());
+            }
+            
+        }
+    }
+}
