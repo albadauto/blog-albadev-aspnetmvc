855d2dbda32b3b162844e6feba959e7d1e12b238
albadev <joseadauto923@gmail.com>
2022-10-06 19:23:02 -0300

    Show posts init

---
+++ Controllers/ShowPostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogDoDautin.Controllers
+{
+    public class ShowPostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
